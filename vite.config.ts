85cef6a93a6af9490f4da051ec374bdaad6f708a
krekro <yuhinw89@gmail.com>
2025-04-16 18:35:12 +0800

    Updated netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import netlifyPlugin from '@netlify/vite-plugin-react-router'
 
 
 export default defineConfig(({ mode }) => {
@@ -11,7 +12,7 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': env
     },
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
     build: {
       target: "esnext",
     }
